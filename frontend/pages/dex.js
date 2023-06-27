fb87caf9223aa3c4ca2f3688dd0c9564f5c7703e
Diego Aguilar <d.aguilaro99@gmail.com>
2023-06-27 16:29:01 +0300

    modified contract connection and styling

--- frontend/pages/dex.js
+++ frontend/pages/dex.js
@@ -1,66 +1,117 @@
 import { useState } from 'react';
-import { useContract, ConnectWallet} from '@thirdweb-dev/react';
+import { useContract, useWallet, useContractWrite, ConnectWallet } from '@thirdweb-dev/react';
 import styles from '../styles/dex.module.css';
 
 export default function DEX() {
-    const contractAddress = '0x8Ba88EE9B7533900cAc23Bac113C0e97a16DCaEf'; // deployed to Sepolia
-    const { contract, isLoading, error } = useContract(contractAddress);
+    const contractAddress = "0x8Ba88EE9B7533900cAc23Bac113C0e97a16DCaEf";
+    const { contract } = useContract(contractAddress);
+    const addLiquidityHook = useContractWrite(contract, "addLiquidity");
+    const removeLiquidityHook = useContractWrite(contract, "removeLiquidity");
+    const ethToTokenSwapHook = useContractWrite(contract, "ethToTokenSwap");
+    const tokenToEthSwapHook = useContractWrite(contract, "tokenToEthSwap");
+    const wallet = useWallet();
     const [status, setStatus] = useState('');
+    const [loadingAction, setLoadingAction] = useState(null);
 
     const addLiquidity = async (amountOfToken) => {
+        if (!amountOfToken) {
+            setStatus('Must enter a value');
+            return;
+        }
         try {
+            setLoadingAction('addLiquidity');
             setStatus('Processing...');
-            const tx = await contract.addLiquidity(amountOfToken, { value: amountOfToken });
-            await tx.wait();
+            await addLiquidityHook.mutateAsync({ args: [amountOfToken] });
             setStatus('Transaction successful');
         } catch (error) {
             setStatus(`Error: ${error.message}`);
+        } finally {
+            setLoadingAction(null);
         }
     };
 
     const removeLiquidity = async (amountOfLPTokens) => {
+        if (!amountOfLPTokens) {
+            setStatus('Must enter a value');
+            return;
+        }
         try {
+            setLoadingAction('removeLiquidity');
             setStatus('Processing...');
-            const tx = await contract.removeLiquidity(amountOfLPTokens);
-            await tx.wait();
+            await removeLiquidityHook.mutateAsync({ args: [amountOfLPTokens] });
             setStatus('Transaction successful');
         } catch (error) {
             setStatus(`Error: ${error.message}`);
+        } finally {
+            setLoadingAction(null);
         }
     };
 
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
+    const ethToTokenSwap = async (minTokensToReceive) => {
+        if (!minTokensToReceive) {
+            setStatus('Must enter a value');
+            return;
+        }
+        try {
+            setLoadingAction('ethToTokenSwap');
+            setStatus('Processing...');
+            await ethToTokenSwapHook.mutateAsync({ args: [minTokensToReceive] });
+            setStatus('Transaction successful');
+        } catch (error) {
+            setStatus(`Error: ${error.message}`);
+        } finally {
+            setLoadingAction(null);
+        }
+    };
 
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
+    const tokenToEthSwap = async (tokensToSwap, minEthToReceive) => {
+        if (!tokensToSwap || !minEthToReceive) {
+            setStatus('Must enter both values');
+            return;
+        }
+        try {
+            setLoadingAction('tokenToEthSwap');
+            setStatus('Processing...');
+            await tokenToEthSwapHook.mutateAsync({ args: [tokensToSwap, minEthToReceive] });
+            setStatus('Transaction successful');
+        } catch (error) {
+            setStatus(`Error: ${error.message}`);
+        } finally {
+            setLoadingAction(null);
+        }
+    };
 
     return (
         <div className={styles.container}>
             <h1>Decentralized Exchange</h1>
-            <ConnectWallet className={styles.connectWallet} />
-            <div>
-                <input type="text" placeholder="Amount of Token" id="amountOfToken" />
-                <button className={styles.button} onClick={() => addLiquidity('addLiquidity', document.getElementById('amountOfToken').value)}>Add Liquidity</button>
-            </div>
-            <div>
-                <input type="text" placeholder="Amount of LP Tokens" id="amountOfLPTokens" />
-                <button className={styles.button} onClick={() => removeLiquidity ('removeLiquidity', document.getElementById('amountOfLPTokens').value)}>Remove Liquidity</button>
-            </div>
-            <div>
-                <input type="text" placeholder="Min Tokens to Receive" id="minTokensToReceive" />
-                <button className={styles.button} onClick={() => executeContractMethod('ethToTokenSwap', document.getElementById('minTokensToReceive').value)}>Swap ETH for Tokens</button>
-            </div>
-            <div>
-                <input type="text" placeholder="Tokens to Swap" id="tokensToSwap" />
-                <input type="text" placeholder="Min ETH to Receive" id="minEthToReceive" />
-                <button className={styles.button} onClick={() => executeContractMethod('tokenToEthSwap', document.getElementById('tokensToSwap').value, document.getElementById('minEthToReceive').value)}>Swap Tokens for ETH</button>
-            </div>
-            <div>
-                <p>{status}</p>
-            </div>
+            {!wallet && <ConnectWallet />}
+            {wallet && (
+                <>
+                <div className={styles.connectWalletContainer}>
+                <ConnectWallet />
+                </div>
+                <div className={styles.action}>
+                    <input type="text" className={styles.input} placeholder="Amount of Token" id="amountOfToken" />
+                    <button className={styles.button} onClick={() => addLiquidity(document.getElementById('amountOfToken').value)}>Add Liquidity</button>
+                </div>
+                <div className={styles.action}>
+                    <input type="text" className={styles.input} placeholder="Amount of LP Tokens" id="amountOfLPTokens" />
+                    <button className={styles.button} onClick={() => removeLiquidity(document.getElementById('amountOfLPTokens').value)}>Remove Liquidity</button>
+                </div>
+                <div className={styles.action}>
+                    <input type="text" className={styles.input} placeholder="Min Tokens to Receive" id="minTokensToReceive" />
+                    <button className={styles.button} onClick={() => ethToTokenSwap(document.getElementById('minTokensToReceive').value)}>Swap ETH for Tokens</button>
+                </div>
+                <div className={styles.action}>
+                    <input type="text" className={styles.input} placeholder="Tokens to Swap" id="tokensToSwap" />
+                    <input type="text" className={styles.input} placeholder="Min ETH to Receive" id="minEthToReceive" />
+                    <button className={styles.button} onClick={() => tokenToEthSwap(document.getElementById('tokensToSwap').value, document.getElementById('minEthToReceive').value)}>Swap Tokens for ETH</button>
+                </div>
+                <div>
+                    <p>{status}</p>
+                </div>
+                </>
+            )}
         </div>
     );
 }
